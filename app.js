6326d7026a4146d0ffe973c4917b127dca27658d
John <johnmiguel.dungao@gmail.com>
2019-07-15 19:25:59 -0400

    toDo-list

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 3000;
-const app = express();
 const path = require("path");
+
+const date = require(__dirname + "/date.js");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(
   bodyParser.urlencoded({
@@ -13,31 +16,37 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 const items = ["Buy Food", "Cook food", "Eat Food"];
+let workItems = [];
 
 app.get("/", (req, res) => {
-  let today = new Date();
-
-  const options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+  let day = date.getDay();
 
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items
   });
 });
 
 app.post("/", (req, res) => {
-  item = req.body.newItem;
-
-  items.push(item);
-  res.redirect("/");
+  let item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", (req, res) => {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
 });
---
+++ date.js
@@ -0,0 +1,25 @@
+getDate = () => {
+  const today = new Date();
+
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+};
+
+getDay = () => {
+  const today = new Date();
+
+  const options = {
+    weekday: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+};
+module.exports = {
+  getDate,
+  getDay
+};
