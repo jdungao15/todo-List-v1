a67889dcaac1ef7cb897deb2e7c99e8ec1b40311
John <johnmiguel.dungao@gmail.com>
2019-07-18 16:37:11 -0400

    Added database. The functionality of removing data and adding data

--- app.js
+++ app.js
@@ -1,51 +1,104 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-
-const date = require(__dirname + "/date.js");
+/* eslint-disable import/no-dynamic-require */
+// import modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// init body-parser
 app.use(
   bodyParser.urlencoded({
-    extended: true
+    extended: true,
   })
 );
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
-const items = ["Buy Food", "Cook food", "Eat Food"];
-let workItems = [];
+// Connecto MongoDB server and create/connect database
+mongoose.connect('mongodb://localhost:27017/todolistDB', {
+  useNewUrlParser: true,
+});
 
-app.get("/", (req, res) => {
-  let day = date.getDay();
+// Item Schema
 
-  res.render("list", {
-    listTitle: day,
-    newListItems: items
+const ItemSchema = {
+  name: String,
+};
+
+// MODEL
+const Item = mongoose.model('Item', ItemSchema);
+
+const item1 = new Item({
+  name: 'Go to Work',
+});
+const item2 = new Item({
+  name: 'Study',
+});
+const item3 = new Item({
+  name: 'Do Homework',
+});
+
+//* *******************//
+//                    //
+//  APP Starts Here  //
+//                  //
+//* ***************//
+
+const defaultItems = [item1, item2, item3];
+const workItems = [];
+
+app.get('/', (req, res) => {
+  Item.find({}, (err, foundItems) => {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, err => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Successfully saved default items to DB.');
+        }
+      });
+      res.redirect('/');
+    } else {
+      res.render('list', {
+        listTitle: 'Today',
+        newListItems: foundItems,
+      });
+    }
   });
 });
 
-app.post("/", (req, res) => {
-  let item = req.body.newItem;
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+app.post('/', (req, res) => {
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name: itemName,
+  });
+
+  item.save();
+  res.redirect('/');
 });
 
-app.get("/work", (req, res) => {
-  res.render("list", { listTitle: "Work List", newListItems: workItems });
+app.post('/delete', (req, res) => {
+  const deletedItemId = req.body.deletedItem;
+
+  Item.findByIdAndRemove(deletedItemId, err => {
+    if (!err) {
+      console.log('Item successfully deleted!');
+      res.redirect('/');
+    }
+  });
 });
 
-app.post("/work", (req, res) => {
-  let item = req.body.newItem;
+app.get('/work', (req, res) => {
+  res.render('list', { listTitle: 'Work List', newListItems: workItems });
+});
+
+app.post('/work', (req, res) => {
+  const item = req.body.newItem;
   workItems.push(item);
-  res.redirect("/work");
+  res.redirect('/work');
 });
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
